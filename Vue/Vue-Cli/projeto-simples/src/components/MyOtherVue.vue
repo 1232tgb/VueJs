5ede13f7d003212285b3fb1a2b1da98926c586c3
1232tgb <48038393+1232tgb@users.noreply.github.com>
2019-11-09 23:11:11 -0300

    Commit inicial

---
+++ Vue/Vue-Cli/projeto-simples/src/components/MyOtherVue.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <div id="image">
+      <img src="../assets/logo.png" alt="imagem" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyOtherVue",
+  data: function() {
+    return {
+      url: "../assets/logo.png"
+    };
+  },
+  computed: {
+    getSource() {
+      return this.url;
+    }
+  }
+};
+</script>
+<style lang="stylus" scoped></style>
---
+++ Vue/Vue-Cli/projeto-simples/src/components/MyVue.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div id="cabecalho">{{ data }}</div>
+    <div>
+      <button id="btn" @click="add">aperte aqui para inserir um {{ message }}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyVue",
+  props: {
+    msg: String
+  },
+  data: function() {
+    return {
+      data: "Esse é o meu trecho.",
+      message: this.msg
+    };
+  },
+  methods: {
+    add() {
+      this.data += " " + this.message;
+    }
+  }
+};
+</script>
+
+<style scoped>
+#cabecalho {
+  background-color: aqua;
+  width: 100%;
+}
+#btn{
+  background-color: darkred;
+  color:white;
+}
+</style>
