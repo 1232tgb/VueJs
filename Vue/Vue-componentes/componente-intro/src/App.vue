fbd3ce8a969e4c31c5db3945ebea8467809176ec
1232tgb <48038393+1232tgb@users.noreply.github.com>
2019-11-10 01:08:01 -0300

    Projeto Vue Cli criado
    Inclusao de novo projeto Vue

---
+++ Vue/Vue-componentes/componente-intro/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Contadores:</h1>
+    <div>{{ contador }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app-vue",
+  data() {
+    return {
+      contador: 0
+    };
+  },
+  methods: {},
+  computed: {}
+};
+</script>
+
+<style>
+</style>
