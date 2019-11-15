36f84b7c3f7f15b123fb856c96f14022f17b9b2c
1232tgb <48038393+1232tgb@users.noreply.github.com>
2019-11-15 00:32:50 -0300

    Criação de nova pasta de estudo
    Estudo de VueJs com VueCli.

---
+++ Vue/Vue-Cli/novo-componente-simples/src/Contador.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="contador">
+    <input type="number" v-model="valorCalculo" />
+
+    <p>
+      valor de calculo:
+      <b>{{valorCalculo}}</b>
+    </p>
+
+    <span>{{ contador }}</span>
+    <button @click="soma()">+</button>
+    <button @click="subtrai()">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "contador",
+  data: function() {
+    return {
+      contador: 0,
+      valorCalculo: 0
+    };
+  },
+  methods: {
+    soma() {
+      this.contador += Number.parseInt(this.valorCalculo);
+    },
+    subtrai() {
+      this.contador -= this.valorCalculo;
+    }
+  }
+};
+</script>]
+
+<style>
+.contador span {
+  border-bottom: 1px solid #ccc;
+  height: 30px;
+  padding: 5px 25px;
+}
+.contador button {
+  height: 30px;
+  width: 30px;
+  border-radius: 15px;
+  background-color: coral;
+  color: #fff;
+  margin-left: 10px;
+  outline: none;
+}
+</style>>
---
+++ Vue/Vue-Cli/novo-componente-simples/src/Contadores.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="Contadores">
+    <h2>{{ nome }}:</h2>
+    <contador v-for="c in 5" :key="c" />
+  </div>
+</template>
+
+<script>
+import contador from "./Contador.vue";
+export default {
+  name: "contadores",
+  data: function() {
+    return {
+      nome: "Contadores"
+    };
+  },
+  components: {
+    contador
+  }
+};
+</script>
